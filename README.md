a0cf1a58eba2a5ea423ec2e395fc67d8dfc59145
falcon <falcon.lin.development@gmail.com>
2024-03-09 20:14:36 +0800

    basic preprocessing

--- README.md
+++ README.md
@@ -12,8 +12,15 @@ Definition:
 -> download basic data
 
 2) preprocessing
+-> remove unsufficient data coins [ i.e. ] may be newly issued coin
+
 
 3) ranking (  )
+outperform in 7 days continuously
+-> for every 15 mins, that's pct_change rank on closing price
+-> plot the rank of pct_change in every 15mins for 7 days. [ step 1s ] 
+
+
 
 4) strategey hypothesis
 
--- utils/now_timestamp.py
+++ utils/now_timestamp.py
@@ -1,9 +1,19 @@
-import time
+from datetime import datetime, timedelta, timezone
 
+def get_timestamp_from_today_midnight_utc(days: int):
+    # Get the current UTC time
+    now_utc = datetime.now(timezone.utc)
+    
+    # Truncate the time to midnight (00:00:00)
+    midnight_utc = now_utc.replace(hour=0, minute=0, second=0, microsecond=0)
+    
+    # Adjust for the specified number of days
+    target_date = midnight_utc + timedelta(days=days)
+    
+    # Convert to timestamp and then to milliseconds
+    midnight_timestamp = int(target_date.timestamp()) * 1000
+    
+    return midnight_timestamp
 
-def get_timestamp_from_now(days: int):
-    return int(time.time()) * 1000 + (days * 24 * 60 * 60 * 1000)
-
-
-def get_now_timestamp():
-    return get_timestamp_from_now(0)
+def get_today_midnight_utc_timestamp():
+    return get_timestamp_from_today_midnight_utc(0)
